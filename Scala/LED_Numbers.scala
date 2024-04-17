762702b3ea99a0485cc1c1e1788c565431c10248
Blag <atejada@gmail.com>
2024-04-16 23:47:06 -0400

    Update LED_Numbers.scala
    Upgrade to Scala 3

--- Scala/LED_Numbers.scala
+++ Scala/LED_Numbers.scala
@@ -1,4 +1,11 @@
-import scala.util.{Try, Success, Failure}
+@main def ledNumbers(num: Int) =
+for( x <- 0 to 2 ) {
+    for( y <- 0 to num.toString.length - 1 ) {
+	val s_num = num.toString
+	print(leds(s_num(y).toString)(x))
+    }
+    println("")
+}
 
 val leds = Map("0" -> List(" _  ","| | ","|_| "),
                "1" -> List("  ","| ","| "),
@@ -10,15 +17,3 @@ val leds = Map("0" -> List(" _  ","| | ","|_| "),
                "7" -> List("_   "," |  "," |  "),
                "8" -> List(" _  ","|_| ","|_| "),
                "9" -> List(" _  ","|_| "," _| "))
-
-val num = scala.io.StdIn.readLine("Please enter an Number: ")
-if(Try(num.toDouble).isSuccess){
-	for( x <- 0 to 2 ) {
-		for( y <- 0 to num.length - 1 ) {
-			print(leds(num(y).toString)(x))
-		}
-		println("")
-	}
-}else{
-	println("Please enter a number!")
-}
